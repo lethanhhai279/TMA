95bc39dffca7de9a285bcd9a75d7397a4bfebe95
Lê Thanh Hai <lethanhhai06122000.ltp@gmail.com>
2022-08-17 22:52:41 +0700

    Update Exam

--- Exam4.py
+++ Exam4.py
@@ -2,12 +2,11 @@ import time
 
 name = str(input("Nhap ten: "))
 current_age = int(input("Nhap tuoi: "))
-# print(type(current_age))
 
 data = time.ctime(time.time())
+# print(data)
 current_year = int(data.split(" ")[-1])
-print(type(current_year))
+# print(type(current_year))
 year_100 = current_year + (100 - current_age)
 
 print(f"Ban {name} se tron 100 tuoi vao nam {year_100}")
-
